5969838d85d8f4f97eb5e8a067768277108071ed
reuzun <moruka12@yandex.com>
2023-01-28 17:01:14 +0300

    feat: add replacing t in the sql date.

---
+++ Reuzun.DB/SQLHelper/SQLDateHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace Reuzun.DB.SQLHelper
+{
+    public class SQLDateHelper
+    {
+
+        public string FixSQLDate(string Date)
+        {
+            return Date.Replace("T", " ");
+        }
+
+    }
+}
---
+++ Reuzun.Run/Program.cs
@@ -0,0 +1,17 @@
+﻿using Reuzun.DB.SQLHelper;
+using System;
+
+namespace Reuzun.Run
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SQLDateHelper SQLHelper = new SQLDateHelper();
+
+            string Date = "2022-12-05T22:22:19";
+
+            Console.WriteLine(SQLHelper.FixSQLDate(Date));
+        }
+    }
+}
