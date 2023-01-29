0ae67a2984f206dce578106ca6e4f37053a083e6
reuzun <moruka12@yandex.com>
2023-01-29 20:19:16 +0300

    feat: add extracting date and clock from datetime object

--- Reuzun.DB/SQLHelper/SQLDateHelper.cs
+++ Reuzun.DB/SQLHelper/SQLDateHelper.cs
@@ -1,12 +1,46 @@
-﻿namespace Reuzun.DB.SQLHelper
+﻿using System;
+
+namespace Reuzun.DB.SQLHelper
 {
-    public class SQLDateHelper
+    public class SQLDateHelper : ISQLDateHelper
     {
 
+        /// <summary>
+        /// Simply replace the 'T' letter in Date.
+        /// </summary>
+        /// <param name="Date"></param>
+        /// <returns>string: Date without including 'T'</returns>
         public string FixSQLDate(string Date)
         {
             return Date.Replace("T", " ");
         }
 
+        /// <summary>
+        /// Returns the date part of DateTime Object.
+        /// E.g 2022.12.12
+        /// Supports YearToDay and DayToYear
+        /// </summary>
+        /// <param name="Date"></param>
+        /// <param name="seperator"></param>
+        /// <param name="DateStyle"></param>
+        /// <returns></returns>
+        public string GetDate(DateTime Date, char seperator = ':', DateStyleEnum DateStyle = DateStyleEnum.DayToYear)
+        {
+            return DateStyle.Equals(DateStyleEnum.DayToYear) ?
+                string.Join(seperator, new int[] { Date.Day, Date.Month, Date.Year }) :
+                string.Join(seperator, new int[] { Date.Year, Date.Month, Date.Day });
+        }
+
+        /// <summary>
+        /// Returns the clock part of DateTime.
+        /// 14.23.18
+        /// </summary>
+        /// <param name="Date"></param>
+        /// <param name="seperator"></param>
+        /// <returns></returns>
+        public string GetClock(DateTime Date, char seperator = ':')
+        {
+            return string.Join(seperator, new int[] { Date.Hour, Date.Minute, Date.Second });
+        }
     }
 }
